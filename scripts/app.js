9c02ddd92222ce015ae398629379165a7a44331c
bradbarrow <bradbarrow@gmail.com>
2015-07-07 21:11:00 +1000

    Wiring the books into our frontend

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,28 @@
+Stamplay.init("bookclub");
+
 var app = angular.module('stamplay', ['ngStamplay']);
 
-app.controller('BooksController', function($scope, $rootScope, $stamplay){
-  $scope.books = [
-    {title: "Harry Potter and the Philosopher's Stone"},
-    {title: "The Hobbit"},
-    {title: "The Perks of Being a Wallflower"}
-  ];
+app.controller('BooksController', function($scope, $rootScope, $stamplay, Book){
+  $scope.books = [];
+
+  Book.all().then(function(books){
+    $scope.books = books;
+  });
+});
+
+app.factory('Book', function($q, $stamplay){
+  function all() {
+    var deferred = $q.defer();
+
+    var BookCollection = $stamplay.Cobject('book').Collection;
+    BookCollection.fetch().then(function() {
+      deferred.resolve(BookCollection.instance);
+    });
+
+    return deferred.promise;
+  }
+
+  return {
+    all: all
+  }
 });
